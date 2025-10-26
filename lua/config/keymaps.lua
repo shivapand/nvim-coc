fe9f024b941ee8e3a96f6afcb1445c40fc3f9b53
shivapand <shiva.pandit@ecolibrium.io>
2025-10-26 16:23:04 +0530

    keeping in sync with nvim-lsp config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,58 +1,74 @@
-vim.keymap.set(
-  { 'i', 'n' },
-  '<C-s>',
-  '<ESC>:silent w<CR>',
-  {
-    desc = 'Save file',
-    silent = true
-  }
-)
+vim.keymap.set({ 'i', 'n' }, '<C-s>', '<ESC>:silent w<CR>', {
+	desc = 'Save file',
+	silent = true
+})
+
+vim.keymap.set('n', '<Leader>/', ':noh<CR>', {
+	desc = 'Clear search highlight',
+	silent = true
+})
 
 vim.keymap.set(
-  'n',
-  '<Leader>/', ':noh<CR>',
-  {
-    desc = 'Clear search highlight',
-    silent = true
-  }
+	'n',
+	'<Leader>g',
+	function()
+		vim.fn.setreg('+', vim.fn.expand('%:~:.'))
+	end,
+	{
+		desc = 'Copy file path',
+		silent = true
+	}
 )
 
-vim.keymap.set(
-  'n',
-  '<Leader>g',
-  function()
-    vim.fn.setreg('+', vim.fn.expand('%:~:.'))
-  end,
-  {
-    desc = 'Copy file path',
-    silent = true,
-  }
-)
+vim.keymap.set('n', '<Leader>j', ':execute "tabmove" tabpagenr() - 2<CR>', {
+	desc = 'Move tab prev'
+})
+vim.keymap.set('n', '<Leader>k', ':execute "tabmove" tabpagenr() + 1<CR>', {
+	desc = 'Move tab next'
+})
 
 vim.keymap.set(
-  'n',
-  '-',
-  '<CMD>Oil --float<CR>',
-  { desc = 'Open parent directory' }
+	'n',
+	'-',
+	function()
+		if vim.bo.filetype == 'nerdtree' then
+			-- If in NERDTree, go up one directory
+			vim.cmd('call nerdtree#ui_glue#invokeKeyMap("u")')
+		else
+			-- If not in NERDTree, toggle it
+			-- Use current directory if no file is open
+			if vim.fn.expand('%') == '' then
+				vim.cmd('NERDTreeToggle .')
+			else
+				vim.cmd('NERDTreeToggle%')
+			end
+		end
+	end,
+	{ desc = 'NERDTree toggle or go up directory' }
 )
 
-vim.keymap.set(
-  'n',
-  '<Leader>j',
-  ':execute "tabmove" tabpagenr() - 2<CR>',
-  { desc = 'Move tab prev' }
-)
-vim.keymap.set(
-  'n',
-  '<Leader>k',
-  ':execute "tabmove" tabpagenr() + 1<CR>',
-  { desc = 'Move tab next' }
-)
+vim.keymap.set('n', '_', ':NERDTree<CR>', { desc = 'NERDTree root directory' })
+
+vim.keymap.set('n', '<C-n>', ':NERDTreeToggle<CR>', {
+	desc = 'Toggle NERDTree'
+})
 
 vim.keymap.set(
-  'n',
-  '<C-n>',
-  ':NERDTreeToggle<CR>',
-  {desc = 'Toggle NERDTree'}
+	'n',
+	'<C-t>',
+	function()
+		if vim.bo.filetype == 'nerdtree' then
+			-- If in NERDTree, send Shift-t instead
+			vim.cmd('call nerdtree#ui_glue#invokeKeyMap("T")')
+
+			vim.cmd('NERDTreeToggle<CR>')
+
+			vim.cmd('tabnext')
+		else
+			-- If not in NERDTree, use normal Ctrl-t behavior
+			vim.cmd('tabnew')
+		end
+	end,
+	{ desc = 'Open in new tab (Ctrl-t in NERDTree, normal tabnew elsewhere)' }
 )
 
--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,16 +1,22 @@
 return {
-  'akinsho/bufferline.nvim',
-  dependencies = {'nvim-tree/nvim-web-devicons'},
-  config = function ()
-    require("bufferline").setup {
-      options = {
-        mode = "tabs",
-        numbers = "ordinal",
-        show_close_icon = false,
-        show_buffer_close_icons = false,
-        diagnostics = 'coc',
-        diagnostics_update_in_insert = true
-      }
-    }
-  end
+	'akinsho/bufferline.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	config = function()
+		require('bufferline').setup{
+			options = {
+				mode = 'tabs',
+				numbers = 'ordinal',
+				show_close_icon = false,
+				show_buffer_close_icons = false,
+				diagnostics = 'nvim_lsp',
+				show_buffer_icons = true,
+				offsets = { {
+					filetype = 'nerdtree',
+					text = 'File Explorer',
+					text_align = 'left',
+					separator = true
+				} }
+			}
+		}
+	end
 }
